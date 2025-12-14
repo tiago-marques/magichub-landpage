a721d64970d4bd246f5faf48f89c220c15f43624
@metiagomarques <tmarquesx@gmail.com>
2025-12-14 01:43:59 +0000

    ci: adiciona workflow do GitHub Actions para deploy automático

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  */
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
+    base: mode === 'production' ? '/magichub-landpage/' : '/',
     plugins: [qwikCity(), qwikVite(), tsconfigPaths({ root: "." })],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
